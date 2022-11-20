10c6c1af8b003702482f2de3f51c7e82571c2077
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-20 17:09:52 +0530

    created card and design the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
+import Card from "../UI/Card";
 
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
 
@@ -7,13 +9,16 @@ const AddUser = (props) => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
-            <label htmlFor="age">Age (Year)</label>
-            <input id="age" type="number"></input>
-            <button type="submit" >Add User</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"></input>
+                <label htmlFor="age">Age (Year)</label>
+                <input id="age" type="number"></input>
+                <button type="submit" >Add User</button>
+            </form>
+        </Card>
+        
     );
 };
 
