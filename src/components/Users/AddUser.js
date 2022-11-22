af48a2bd5e946dcb59f3201234e82714f5e71d60
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-22 19:03:00 +0530

    listenting input and taking action based on that

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
@@ -5,17 +6,29 @@ import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
 
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (e) =>{
         e.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    }
+
+    const usernameHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageHandler = (event) =>{
+        setEnteredAge(event.target.value);
     }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"></input>
+                <input id="username" type="text" onChange={usernameHandler}></input>
                 <label htmlFor="age">Age (Year)</label>
-                <input id="age" type="number"></input>
+                <input id="age" type="number" onChange={ageHandler}></input>
                 <Button type="submit">Add User</Button>
                 
             </form>
