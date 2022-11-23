5451fa27cdf1674b77986a59fc816a1a47d31145
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-23 15:03:19 +0530

    creating an error modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,19 +5,19 @@ import classes from './ErrorModal.module.css';
 const ErrorModal =(props) =>{
     return (
         <div>
-        <div className={classes.breakdrop}>
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={classes.content}>
-                <p>{props.message}</p>
+            <div className={classes.breakdrop} onClick={props.onConfirm}>
+             <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <button onClick={props.onConfirm}>Okay</button>
+                </footer>
+              </Card>
             </div>
-            <footer className={classes.actions}>
-                <button>Okay</button>
-            </footer>
-        </Card>
-        </div>
         </div>
     );
 };
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 
 import styles from './AddUser.module.css';
 
@@ -8,13 +9,23 @@ const AddUser = (props) => {
 
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [actualError, setActualError] = useState('');
 
     const addUserHandler = (e) =>{
         e.preventDefault();
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            setActualError({
+                title: "Invalid Input",
+                message: "Please enter a valid name and age (non-empty values).",
+            });
             return;
+
         }
         if(+enteredAge < 1){
+            setActualError({
+                title: "Invalid Age",
+                message: "Please enter a valid age (> 0).",
+            });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -31,7 +42,13 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     }
 
+    const errorHandler = () => {
+        setActualError(null);
+    }
+
     return (
+        <div>
+        {actualError && <ErrorModal title={actualError.title} message={actualError.message} onConfirm={errorHandler}/>}
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
@@ -42,6 +59,7 @@ const AddUser = (props) => {
                 
             </form>
         </Card>
+        </div>
         
     );
 };
