ad05919503e9de86dc13048b759e0c6e67924db9
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-23 12:50:53 +0530

    creating an error modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Card from './Card';
+import classes from './ErrorModal.module.css';
 
 const ErrorModal =(props) =>{
     return (
-        <Card>
-            <header>
+        <div>
+        <div className={classes.breakdrop}>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
                 <h2>{props.title}</h2>
             </header>
-            <div>
+            <div className={classes.content}>
                 <p>{props.message}</p>
             </div>
-            <footer>
+            <footer className={classes.actions}>
                 <button>Okay</button>
             </footer>
         </Card>
-        
+        </div>
+        </div>
     );
 };
 
